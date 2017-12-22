9b7c79dabbbcb714531fc4b397a405a7356271a0
jyash97 <jyash97@gmail.com>
2017-12-22 19:54:52 +0530

    ES6 Updated Files

---
+++ a.js
@@ -0,0 +1,13 @@
+let  a = 'Yah Josh';
+a++;
+alert(a);
+
+/*
+
+  If you are using Atom
+
+  Install Linter and Liner-eslint and if it asks for other dependicies install them.
+
+  Then replace single comma to double inverted comma it give an error which on saving fixes automatically.
+
+*/
