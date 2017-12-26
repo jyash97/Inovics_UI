4241e16abe421e9ae15b827432a798c9149737fc
jyash97 <jyash97@gmail.com>
2017-12-26 22:10:16 +0530

    Eslint Precommit added

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -3,46 +3,51 @@ import React from 'react';
 import Links from './Links';
 import Profile from './images/profile.jpg';
 
-class Sidebar extends React.Component{
-
-  constructor(){
+class Sidebar extends React.Component {
+  constructor() {
     super();
-    this.state={
-      toggle:true
+    this.state = {
+      toggle: true
     };
     this.handleWidth = this.handleWidth.bind(this);
   }
 
-  handleWidth(){
+  handleWidth() {
     this.setState({
-      toggle:!this.state.toggle
+      toggle: !this.state.toggle
     });
   }
 
-  render(){
+  render() {
     let stylesSidebar = {
-      'width':'19vw',
-      'height':'91vh'
+      width: '19vw',
+      height: '91vh'
     };
-    if(!this.state.toggle){
+    if (!this.state.toggle) {
       stylesSidebar = {
-        'width':'2vw',
-        'height':'5vh'
+        width: '2vw',
+        height: '5vh'
       };
     }
     // Profile will be uploaded via Backend just for UI purpose.
     const stylesProfile = {
-      'width': '9vw',
-      'height': '9vw',
-      'backgroundImage': `url(${Profile})`,
-      'backgroundSize': 'cover',
-      'backgroundPosition': 'top center'
+      width: '9vw',
+      height: '9vw',
+      backgroundImage: `url(${Profile})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'top center'
     };
-    return(
+    return (
       <div className="sidebar float-left" style={stylesSidebar}>
-        <span className="float-right p-2" role="img" aria-label="Snowman" onClick={this.handleWidth}>❤️</span>
-        <div className="mx-auto my-4 rounded-circle" style={stylesProfile}>
-        </div>
+        <span
+          className="float-right p-2"
+          role="img"
+          aria-label="Snowman"
+          onClick={this.handleWidth}
+        >
+          ❤️
+        </span>
+        <div className="mx-auto my-4 rounded-circle" style={stylesProfile} />
         <Links />
       </div>
     );
