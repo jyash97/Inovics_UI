574b0dea47ea910f9dd5f3ebff3a256b695efe0b
jyash97 <jyash97@gmail.com>
2018-02-10 12:49:56 +0530

    Ignore this commit

--- src/Help.jsx
+++ src/Help.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-export default class Help extends React.Component{
-  render(){
-    console.log('errr');
-    return(
-      <h1>Hello Help</h1>
-    );
+export default class Help extends React.Component {
+  render() {
+    return <h1>Hello Help</h1>;
   }
 }
