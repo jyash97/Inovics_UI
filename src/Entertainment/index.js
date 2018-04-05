7f9c1c38dc7b849f3f6cc06fe433f742cc5eeb5d
sachin <sachinbansode18@gmail.com>
2018-04-06 01:04:16 +0530

    Added Whack A Mole to Entertainment Component

--- src/Entertainment/index.js
+++ src/Entertainment/index.js
@@ -24,6 +24,13 @@ class Entertainment extends React.Component {
       linktitle: 'Play Now',
       link: `${process.env.PUBLIC_URL}/drumkit/index.html`
     });
+    dataComponent.push({
+      title: 'Whack A Mole',
+      time: Date.now(),
+      image: `${process.env.PUBLIC_URL}/images/entertainment2.jpg`,
+      linktitle: 'Play Now',
+      link: `${process.env.PUBLIC_URL}/whack-a-mole/index.html`
+    });
     this.setState({
       data: dataComponent
     });
